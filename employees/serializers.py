e801d8925238a584d4d0f2fd295cee2a0a01e181
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-11-21 12:25:50 +0500

    bug fix

--- employees/serializers.py
+++ employees/serializers.py
@@ -64,6 +64,7 @@ class AttendancesModelSerializer(serializers.ModelSerializer):
         month = request.GET.get("month")
         year = request.GET.get("year")
         access_control = AccessControl.objects.filter(employee=obj, created__day=day, created__month=month, created__year=year)
+        print(access_control)
         if access_control:
             access_control = access_control.last()
             return access_control.status
