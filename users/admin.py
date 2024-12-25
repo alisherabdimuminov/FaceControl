50e70db98136622c8f624cacde1bf97242292cab
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-12-25 16:26:52 +0500

    bug fix

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from .models import History, User
 
 
-from .models import History
+@admin.register(User)
+class UserModelAdmin(UserAdmin):
+    add_form = UserCreationModeForm
+    form = UserChangeModelForm
+    list_display = ["username", "first_name", "last_name"]
+    model = User
+    fieldsets = (
+        ("Foydalanuvchini tahrirlash", {
+            "fields": ("username", "first_name", "last_name", )
+        }), 
+    )
+    add_fieldsets = (
+        ("Yangi foydalanuvchi qo'shish", {
+            "fields": ("username", "password1", "password2", "first_name", "last_name", )
+        }), 
+    )
 
 
 @admin.register(History)
